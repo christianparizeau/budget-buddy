32faffacff4ecc52a94cbfa1ede4315a5d5024c2
Christian <christianparizeau@gmail.com>
2020-03-10 12:36:44 -0700

    inital commit of dependencies and dev dependencies

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+    mode: 'none',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        plugins: [
+                            '@babel/plugin-transform-block-scoping',
+                            '@babel/plugin-transform-arrow-functions'
+                        ]
+                    }
+                }
+            }
+        ]
+    }
+};
